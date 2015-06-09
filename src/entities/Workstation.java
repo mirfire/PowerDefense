e90a138e2a541a6a9dc410e6505826de727db4e4
Araxar <gandossi.luigi@gmail.com>
2015-06-09 17:27:47 +0200

    cvbn

--- src/entities/Workstation.java
+++ src/entities/Workstation.java
@@ -2,12 +2,16 @@ package entities;
 
 public class Workstation {
 	private static int maxHp = 100;
+	private static int life = 100;
 	
-	public static int getMaxHp() {
-		return maxHp;
+	public static int getlife() {
+		return life;
 	}
 
-	public static void setMaxHp(int maxHp) {
-		maxHp = maxHp;
+	public static void setlife(int life) {
+		life = life;
+	}
+	public void repair(int life, int ressources){
+		return;
 	}
 }
