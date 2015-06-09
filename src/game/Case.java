6fd1bfed1d75981c7f9f595f4828438f2d96423d
Rockfire <rocknfire@orange.fr>
2015-06-09 10:34:19 +0200

    Finition de la classe Case

--- src/game/Case.java
+++ src/game/Case.java
@@ -5,8 +5,35 @@ public class Case {
 	private boolean buildable;
 	private String spritePath;
 	
-	public Case(int x, int y) {
+	public Case(int x, int y, String spritePath) {
 		this.coords.setX(x);
 		this.coords.setY(y);
+		this.buildable = true;
+		this.spritePath = spritePath;
 	}
+
+	public Coords getCoords() {
+		return coords;
+	}
+
+	public void setCoords(Coords coords) {
+		this.coords = coords;
+	}
+
+	public boolean isBuildable() {
+		return buildable;
+	}
+
+	public void setBuildable(boolean buildable) {
+		this.buildable = buildable;
+	}
+
+	public String getSpritePath() {
+		return spritePath;
+	}
+
+	public void setSpritePath(String spritePath) {
+		this.spritePath = spritePath;
+	}
+	
 }
