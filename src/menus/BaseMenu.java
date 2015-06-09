b6e17a07be7de04d77910645fc4a5d18cfebfc03
Rockfire <rocknfire@orange.fr>
2015-06-09 15:23:50 +0200

    Ajout d'une classe abstraite pour les Menus Travail continué sur les menus

---
+++ src/menus/BaseMenu.java
@@ -0,0 +1,28 @@
+package menus;
+
+import javax.swing.JDialog;
+import javax.swing.JFrame;
+import javax.swing.JOptionPane;
+import javax.swing.JPanel;
+import javax.swing.SwingUtilities;
+
+public abstract class BaseMenu extends JPanel {
+	JOptionPane dlgConfirmExitGame;
+	
+	public void backToMainMenu() {
+		Window.LayoutManager.show(getParent(), Menus.MAIN.toString());
+	}
+	
+	public void showNewGameMenu() {
+		Window.LayoutManager.show(getParent(), Menus.NEWGAME.toString());
+	}
+	
+	public void showLoadGameMenu() {
+		Window.LayoutManager.show(getParent(), Menus.LOADGAME.toString());
+	}
+	
+	public void showConfirmExitGameDialog() {
+		int n = JOptionPane.showConfirmDialog((JFrame) SwingUtilities.getRoot(this), "Voulez-vous vraiment quitter ?", "", JOptionPane.YES_NO_OPTION);
+		// TODO: exitGame();
+	}
+}
