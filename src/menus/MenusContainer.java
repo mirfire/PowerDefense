2c1822197f5c9e7243968edb0eb79bf024aefe85
Ollocip <nicolas.francois1@outlook.fr>
2015-06-10 15:40:02 +0200

    Yoo

--- src/menus/MenusContainer.java
+++ src/menus/MenusContainer.java
@@ -1,5 +1,6 @@
 package menus;
 
+import java.awt.Color;
 import java.awt.Frame;
 import java.awt.Graphics;
 
@@ -8,6 +9,7 @@ import javax.swing.JPanel;
 public class MenusContainer extends JPanel implements Runnable {
 	
 	Thread menuThread =  new Thread(this);
+	Frame windowFrame;
 	
 	private final long DELAY = 1;
 	private int FPS = 0; 
@@ -15,9 +17,49 @@ public class MenusContainer extends JPanel implements Runnable {
 
 	public MenusContainer(Frame frame) {
 		menuThread.start();
+		this.windowFrame = frame;
 	}
+	
+
+	
+	public void paintComponent(Graphics g) {
+		drawFPS(g);		
+		
+		}
+	
 	
 	public void run() {
 		System.out.println("Menus lancés");
+		
+		long lastFrame = System.currentTimeMillis();
+		
+		// Boucle de l'interface
+		while(true) {			
+			try {
+				Thread.sleep(DELAY);
+			} catch (InterruptedException e) {
+				e.printStackTrace();
+			}
+		}
 	}
+	
+	private void drawFPS(Graphics g) {
+		g.clearRect(0, 0, this.windowFrame.getWidth(), this.windowFrame.getHeight());
+		g.drawString(FPS + "FPS", 1, 10);
+		
+		
+	}
+	
+	private long countFPS(long lastFrame) {
+		frames++;
+		if(System.currentTimeMillis() - 1000 >= lastFrame) {
+			this.FPS = frames;
+			this.frames = 0;
+			lastFrame = System.currentTimeMillis();
+		}
+		return lastFrame;
+		
+	}
+
+	
 }
