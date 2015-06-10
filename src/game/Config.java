ad595d2d87d9081f0c3f5e2ef5ce4403c07623e8
Rockfire <rocknfire@orange.fr>
2015-06-10 19:26:28 +0200

    Passage en privé d'une partie de l'URL de la DB

--- src/game/Config.java
+++ src/game/Config.java
@@ -5,9 +5,9 @@ public class Config {
 	// Base de Données
 	public final static String DB_USER 			= "root";
 	public final static String DB_PASS 			= "";
-	public final static String DB_URL 				= "127.0.0.1";
-	public final static String DB_PORT				= "3306";
-	public final static String DATABASE			= "projet_java";
+	private final static String DB_URL 				= "127.0.0.1";
+	private final static String DB_PORT				= "3306";
+	private final static String DATABASE			= "projet_java";
 	public final static String DB_CONNECTION_URL 	= "jdbc:mysql://" + DB_URL + ":" + DB_PORT + "/" + DATABASE;
 	
 	// Jeu
