4de54ba3aaedc9644be20a24ac3d041b6d22da77
Rockfire <rocknfire@orange.fr>
2015-06-10 21:00:48 +0200

    Ajout du constructeur et des coordonnées à Workstation()

--- src/entities/Workstation.java
+++ src/entities/Workstation.java
@@ -1,29 +1,40 @@
 package entities;
 
+import game.Coords;
+
 public class Workstation {
 	public static Workstation workstation = new Workstation();
+	private Coords coords;
 	private int maxHp = 100;
 	private int life = 100;
 	
+	public Workstation() {
+		coords = new Coords(0,0);
+	}
+	
+	public Coords getCoords() {
+		return coords;
+	}
+
+	public void setCoords(Coords coords) {
+		this.coords = coords;
+	}
+
 	public int getMaxHp() {
 		return maxHp;
 	}
-
 
 	public void setMaxHp(int maxHp) {
 		this.maxHp = maxHp;
 	}
 
-
 	public int getLife() {
 		return life;
 	}
 
-
 	public void setLife(int life) {
 		this.life = life;
 	}
-
 
 /*	public void repair(int life, int ressources){
 		btnRepair.addActionListener(new ActionListener() {
