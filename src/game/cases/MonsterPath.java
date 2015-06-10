8010ba76fbcb99b74b8ff0814cf2735e79ecca85
Rockfire <rocknfire@orange.fr>
2015-06-10 17:10:58 +0200

    Ajout des sous classes de Case

---
+++ src/game/cases/MonsterPath.java
@@ -0,0 +1,24 @@
+package game.cases;
+
+import game.Case;
+
+import java.io.File;
+import java.io.IOException;
+
+import javax.imageio.ImageIO;
+
+public class MonsterPath extends Case{
+
+	public MonsterPath(int x, int y) {
+		super(x, y);
+		this.buildable = false;
+		this.monsterPath = true;
+		this.spritePath = "resources/sprites/dirt.png";
+		try {
+			this.sprite = ImageIO.read(new File(this.spritePath));
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+	}
+
+}
