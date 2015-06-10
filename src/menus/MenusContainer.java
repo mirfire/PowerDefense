a1ceee554713123a20e2b9c58b174c1e0eb1bf80
Rockfire <rocknfire@orange.fr>
2015-06-10 20:51:06 +0200

    Nettoyage du MenuContainer

--- src/menus/MenusContainer.java
+++ src/menus/MenusContainer.java
@@ -6,17 +6,9 @@ import java.awt.Graphics;
 import javax.swing.JPanel;
 
 public class MenusContainer extends JPanel implements Runnable {
-	
-	/**
-	 * 
-	 */
 	private static final long serialVersionUID = 871294247435865586L;
 	Thread menuThread =  new Thread(this);
 	Frame windowFrame;
-	
-	private final long DELAY = 1;
-	private int FPS = 0; 
-	int frames = 0;
 
 	public MenusContainer(Frame frame) {
 		menuThread.start();
@@ -25,25 +17,5 @@ public class MenusContainer extends JPanel implements Runnable {
 	
 	public void run() {
 		System.out.println("Menus lancés");
-	}
-	
-	private void drawFPS(Graphics g) {
-		g.clearRect(0, 0, this.windowFrame.getWidth(), this.windowFrame.getHeight());
-		g.drawString(FPS + "FPS", 1, 10);
-		
-		
-	}
-	
-	private long countFPS(long lastFrame) {
-		frames++;
-		if(System.currentTimeMillis() - 1000 >= lastFrame) {
-			this.FPS = frames;
-			this.frames = 0;
-			lastFrame = System.currentTimeMillis();
-		}
-		return lastFrame;
-		
-	}
-
-	
+	}	
 }
