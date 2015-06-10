535332301eaf2c75b9abf0f85dbbe749c36ec4af
Rockfire <rocknfire@orange.fr>
2015-06-10 14:31:16 +0200

    Suppression de code redondant

--- src/menus/MenusContainer.java
+++ src/menus/MenusContainer.java
@@ -8,7 +8,6 @@ import javax.swing.JPanel;
 public class MenusContainer extends JPanel implements Runnable {
 	
 	Thread menuThread =  new Thread(this);
-	Frame windowFrame;
 	
 	private final long DELAY = 1;
 	private int FPS = 0; 
@@ -16,42 +15,9 @@ public class MenusContainer extends JPanel implements Runnable {
 
 	public MenusContainer(Frame frame) {
 		menuThread.start();
-		this.windowFrame = frame;
-	}
-	
-	public void paintComponent(Graphics g) {
-		drawFPS(g);		
 	}
 	
 	public void run() {
 		System.out.println("Menus lancés");
-		
-		long lastFrame = System.currentTimeMillis();
-		
-		// Boucle de l'interface
-		while(true) {			
-			try {
-				Thread.sleep(DELAY);
-			} catch (InterruptedException e) {
-				e.printStackTrace();
-			}
-		}
 	}
-	
-	private void drawFPS(Graphics g) {
-		g.clearRect(0, 0, this.windowFrame.getWidth(), this.windowFrame.getHeight());
-		g.drawString(FPS + "FPS", 1, 10);
-	}
-	
-	private long countFPS(long lastFrame) {
-		frames++;
-		if(System.currentTimeMillis() - 1000 >= lastFrame) {
-			this.FPS = frames;
-			this.frames = 0;
-			lastFrame = System.currentTimeMillis();
-		}
-		return lastFrame;
-		
-	}
-
 }
